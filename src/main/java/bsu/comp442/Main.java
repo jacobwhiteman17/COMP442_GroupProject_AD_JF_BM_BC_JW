495359fd3418c298c219f8d24501e0650651b538
Jacob <j1whiteman@student.bridgew.edu>
2020-12-07 21:31:49 -0500

    New build.gradle

--- src/main/java/bsu/comp442/Main.java
+++ src/main/java/bsu/comp442/Main.java
@@ -8,14 +8,9 @@ public class Main {
 
     public static void main(String[] args)
     {
-        final String url = "https://jobs.bridgew.edu/postings/search";
-        try {
-            final Document document = Jsoup.connect(url).get();
-            //for(Element row: document.select())
-            //System.out.println(document.outerHtml());
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        System.out.println("Test");
     }
+
+
 
 }
